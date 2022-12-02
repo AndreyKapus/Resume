8e539c2d18680f2a3cb0b810307f308cd019e677
AndreyKapus <kapustnikov@ukr.net>
2022-12-02 23:35:13 +0200

    contactsInfo

---
+++ src/components/ContactInfo/ContactInfo.js
@@ -0,0 +1,22 @@
+const ContactInfo = () => {
+  return (
+    <div>
+      <h2>Contact information</h2>
+      <ul>
+        <li>
+          <a href="tel:+380934066204">+380934066204</a>
+        </li>
+        <li>
+          <a href="mailto:andriikapustnikov@gmail.com">
+            andriikapustnikov@gmail.com
+          </a>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactInfo;
